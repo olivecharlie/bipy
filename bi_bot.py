3c4d83bd1e14bd0b7afa1f6c8665aa2d1ce61ad1
Luke Munro <luke.munro@gmail.com>
2019-03-04 22:34:13 +0000

    initial bipy commit

---
+++ bi_bot.py
@@ -0,0 +1,123 @@
+import bipy as bp
+import json
+import datetime
+import time
+
+def ping_time(client):
+	timestamp = client.ping_time()
+	td = datetime.datetime.fromtimestamp(int(timestamp['serverTime'])/1000)
+	print('Connected... server time is:', td)
+
+def get_min_order_id(orders):
+	if(len(orders) == 0):
+		return 0
+	ids = []
+	for o in orders:
+		ids.append(int(o['orderId']))
+	return min(ids)
+
+def order_difference_new(order_watch_list, orders):
+	added = 0
+	new_orders = filter(lambda o: o['status']==bp.BiPy.ORDER_STATUS_NEW, orders)
+	for o in new_orders:
+		id = o['orderId']
+		if(len(order_watch_list) > 0):
+			f = list(filter(lambda i: i['orderId'] == id, order_watch_list))
+			if(len(f) == 0):
+				print(f'New order {id} detected, adding...')
+				order_watch_list.append(o)
+				added += 1
+		else:
+			print(f'Adding first order: {id}...')
+			order_watch_list.append(o)
+			added += 1
+	if(added > 0):
+		print(f'{added} new orders added to watch list...')
+	else:
+		print('No new orders detected.')
+		
+#def order_difference_filled(order_watch_list, orders):
+	#filled_orders = 
+			
+def get_orders(client, order_watch_list, **kwargs):
+	'''
+		filter e.g. status='NEW'
+		ORDER:
+		symbol : TRXBTC
+		orderId : 95518149
+		clientOrderId : ios_f4576fe09e094614af7a6c88773af717
+		price : 0.00000625
+		origQty : 3272.00000000
+		executedQty : 3272.00000000
+		cummulativeQuoteQty : 0.02045000
+		status : FILLED
+		timeInForce : GTC
+		type : LIMIT
+		side : BUY
+		stopPrice : 0.00000000
+		icebergQty : 0.00000000
+		time : 1551303552705
+		updateTime : 1551319830936
+		isWorking : True
+		
+	'''
+	orders = client.get_all_orders(**kwargs)
+	print(f'Orders recieved in query: {len(orders)}')
+	#check for new orders
+	order_difference_new(order_watch_list, orders)
+	
+def order_print(orders):
+		print(f'The current, tracked order list is:	')
+		for o in orders:
+			print('ORDER:')
+			for i in o:
+				print(f'	{i} : {o[i]}')
+
+def process_key_press(key_press):
+	if key_press.ToUpper() == 'L':
+		order_print()
+
+
+
+def main():
+	#constants and config
+	POLL_INTERVAL_SECONDS = 5
+	TRADING_PAIR = 'TRXBTC'
+	#load config
+	with open('_config.json', 'r') as f:
+		config = json.load(f)
+
+	#logon to binance
+	secret = config['DEFAULT']['SECRET_KEY']
+	key = config['DEFAULT']['API_KEY']
+	#create client
+	client = bp.BiPy(key, secret)
+	order_watch_list = []
+	#ping test
+	ping_time(client)
+	#load orders into watch-list
+	get_orders(client, order_watch_list, symbol=TRADING_PAIR)
+	first_order = get_min_order_id(order_watch_list)
+	print(f'First open order id is: {first_order}. All orders will be fetched in respct of this until restart.')
+		
+	#trade
+	while True:
+		ping_time(client)	
+		#check orders
+		get_orders(client, order_watch_list, symbol=TRADING_PAIR, orderId=str(first_order))
+		#update watch list
+		
+		#evalaute watchlist
+		price = client.get_price(symbol=TRADING_PAIR)['price']
+		print(f'Current {TRADING_PAIR} price: {price}')
+		#print(f'current {TRADING_PAIR} price: {price[1]}')
+		#order_print(current_orders)
+		#place orders
+		time.sleep(POLL_INTERVAL_SECONDS)
+
+if (__name__ == '__main__'):
+	main()
+
+	
+
+
---
+++ bipy_test.py
@@ -0,0 +1,45 @@
+#https://docs.python.org/2/library/unittest.html
+import unittest
+import bipy
+
+class BiPyTests(unittest.TestCase):
+	
+	RESULT_PARAMETER_LIST = 'symbol=TRXBTC&timestamp=1550964774431'
+	RESULT_SIGNED_PARAMETER_LIST = 'cdc1600741ccb157765fc3e8c2fb35a04ef1fed472744d074bff7b12888aa4a0'
+	REGEX_GET_URI_SIGNED = 'https:\/\/api\.binance\.com\/api\/v3\/allOrders\?symbol=TRXBTC&timestamp=\d{13}&signature=[a-f0-9]{64}'
+	RESULT_GET_URI_UNSIGNED = 'https://api.binance.com/api/v1/depth?symbol=TRXBTC&limit=20'
+	RESULT_GET_URI_UNSIGNED_NO_PARAMETERS = 'https://api.binance.com/api/v3/ticker/price?'
+	REGEX_GET_ALL_ORDERS = 'https:\/\/api\.binance\.com\/api\/v3\/allOrders\?symbol=TRXBTC&timestamp=\d{13}&signature=[a-f0-9]{64}'
+	REGEX_TIMESTAMP = '\d{13}'
+	
+	def test__get_headers(self):
+		sut = bipy.BiPy('key', 'secret')
+		self.assertEqual(sut._BiPy__get_headers(), { 'X-MBX-APIKEY' : 'key' })
+		
+	def test__get_parameters(self):
+		sut = bipy.BiPy('key', 'secret')
+		self.assertEqual(sut._BiPy__get_parameters(symbol = 'TRXBTC', timestamp=1550964774431), self.RESULT_PARAMETER_LIST)
+						
+	def test__sign_request(self):
+		sut = bipy.BiPy('key', 'secret')
+		self.assertEqual(sut._BiPy__sign_request(self.RESULT_PARAMETER_LIST), self.RESULT_SIGNED_PARAMETER_LIST)
+		
+	def test__get_uri_when_signed(self):
+		sut = bipy.BiPy('key', 'secret')
+		self.assertRegexpMatches(sut._BiPy__get_uri(bipy.BiPy.ENDPOINT_ALL_ORDERS, True, symbol = 'TRXBTC'), self.REGEX_GET_URI_SIGNED)
+		
+	def test__get_uri_when_unsigned(self):
+			sut = bipy.BiPy('key', 'secret')
+			self.assertEqual(sut._BiPy__get_uri(bipy.BiPy.ENDPOINT_DEPTH, False, symbol = 'TRXBTC', limit=20), self.RESULT_GET_URI_UNSIGNED)
+			
+	def test__get_uri_when_unsigned_no_paramters(self):
+			sut = bipy.BiPy('key', 'secret')
+			self.assertEqual(sut._BiPy__get_uri(bipy.BiPy.ENDPOINT_PRICE, False), self.RESULT_GET_URI_UNSIGNED_NO_PARAMETERS)
+			
+	#def test_ping_time(self):
+	#	sut = bipy.BiPy('key', 'secret')
+	#	self.assertRegexpMatches(sut.ping_time()), self.REGEX_GET_URI_SIGNED)	
+	
+	
+suite = unittest.TestLoader().loadTestsFromTestCase(BiPyTests)
+unittest.TextTestRunner(verbosity=2).run(suite)
